bb342b83f129b40d7068b8ff6dea06a129e46e4a
Mateusz Siedlecki <siedleckimateus@gmail.com>
2021-07-21 16:49:33 +0200

    First commit with new Project

---
+++ src/main/java/com/siedlecki/mateusz/gacek/GacekApplication.java
@@ -0,0 +1,13 @@
+package com.siedlecki.mateusz.gacek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GacekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GacekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siedlecki/mateusz/gacek/GacekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siedlecki.mateusz.gacek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GacekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
