8fdc481ba7bb970c7ffeff07e23718d499043bc5
Mateusz Siedlecki <siedleckimateus@gmail.com>
2022-09-08 20:20:57 +0200

    changes column in excel file

--- src/main/java/com/siedlecki/mateusz/gacek/core/SheetContentGenerator.java
+++ src/main/java/com/siedlecki/mateusz/gacek/core/SheetContentGenerator.java
@@ -8,12 +8,42 @@ import java.util.List;
 public class SheetContentGenerator {
 
     public static String[] TO_PREPARE_COLUMNS = new String[]{
-            "SPECSHOP","RANGE GR","ART NR","ART NAME","SLID","PRENOT ORDER[PQ]","L23 ORDER[PQ]"
+            "OBSZAR","REG","NR","NAME","SLID","PALETY","STATUS"
     };
 
+    public static List<String[]> toPrepareSheetValues(List<IkeaProduct> products){
+        List<String[]> result = new ArrayList<>();
+        for (IkeaProduct product : products){
+            result.add(new String[]{
+                    product.getMainLocation().getSpecshop(),
+                    product.getMainLocation().getRangeGroup(),
+                    product.getId(),
+                    product.getName(),
+                    product.locationsToString(),
+                    String.format("%.2f", (product.prenotSalesPQ()+(double)product.l23OrderToFullPal())),
+                    product.getStatus().getOpis()
+            });
+        }
+        return result;
+    }
+
     public static String[] TO_ORDER_COLUMNS = new String[]{
-            "ART NR","ART NAME","SLID","PRENOT ORDER[PQ]","L23 ORDER[PQ]"
+            "ART NR","NAZWA","PRENOT[pal]","L23[pal]","SUMA[szt]"
     };
+
+    public static List<String[]> toOrderSheetValues(List<IkeaProduct> products){
+        List<String[]> result = new ArrayList<>();
+        for (IkeaProduct product : products){
+            result.add(new String[]{
+                    product.getId(),
+                    product.getName(),
+                    String.format("%.2f", product.prenotSalesPQ()),
+                    String.valueOf(product.l23OrderToFullPal()),
+                    String.valueOf((product.l23Order()+product.getPrenotSales()))
+            });
+        }
+        return result;
+    }
 
     public static String[] ALL_PARAM_COLUMNS = new String[]{
             "SPECSHOP","RANGE GR","ART NR","ART NAME","SLID","FCST","ASSQ","AVG SALES","PQ","AVAL STOCK",
@@ -21,45 +51,15 @@ public class SheetContentGenerator {
             "ON SALES PLACES","FREE SPACE","L23 QTY","L23[PQ]","L23 [FULL PQ]","FREE SPACE AFTER ORD","RESERVATIONS"
     };
 
-    public static List<String[]> toPrepareSheetValues(List<IkeaProduct> products){
-        List<String[]> result = new ArrayList<>();
-        for (IkeaProduct product : products){
-            result.add(new String[]{
-                    product.getSpecshop(),
-                    product.getRangeGroup(),
-                    product.getNumberId(),
-                    product.getName(),
-                    product.getLocations().toString(),
-                    String.format("%.2f", product.prenotSalesPQ()),
-                    String.valueOf(product.l23OrderToFullPal())
-            });
-        }
-        return result;
-    }
-
-    public static List<String[]> toOrderSheetValues(List<IkeaProduct> products){
-        List<String[]> result = new ArrayList<>();
-        for (IkeaProduct product : products){
-            result.add(new String[]{
-                    product.getNumberId(),
-                    product.getName(),
-                    product.getLocations().toString(),
-                    String.format("%.2f", product.prenotSalesPQ()),
-                    String.valueOf(product.l23OrderToFullPal())
-            });
-        }
-        return result;
-    }
-
     public static List<String[]> allParamSheetValues(List<IkeaProduct> products){
         List<String[]> result = new ArrayList<>();
         for (IkeaProduct product : products){
             result.add(new String[]{
-                    product.getSpecshop(),
-                    product.getRangeGroup(),
-                    product.getNumberId(),
+                    product.getMainLocation().getSpecshop(),
+                    product.getMainLocation().getRangeGroup(),
+                    product.getId(),
                     product.getName(),
-                    product.getLocations().toString(),
+                    product.locationsToString(),
                     product.getFcst().toString(),
                     product.getAssq().toString(),
                     String.format("%.2f", product.getAvgSales()),
