8718fbbae74ffe6d55c7fa1f619ef1780ecf8d90
Mateusz <siedleckimateus@gmail.com>
2023-11-23 19:48:39 +0100

    added error message to the model attribute

--- src/main/java/com/siedlecki/mateusz/gacek/GacekApplication.java
+++ src/main/java/com/siedlecki/mateusz/gacek/GacekApplication.java
@@ -2,6 +2,7 @@ package com.siedlecki.mateusz.gacek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
@@ -13,7 +14,8 @@ public class GacekApplication {
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public String handleFileSizeLimitExceeded(MaxUploadSizeExceededException exc) {
+    public String handleFileSizeLimitExceeded(MaxUploadSizeExceededException exc, Model model) {
+        model.addAttribute("errorMsg", exc.getMessage());
         return "redirect:/error";
     }
 
